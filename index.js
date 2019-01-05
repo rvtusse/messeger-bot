5add4d179786a5effeb5f162ba8386902dfd2535
herve <herve@thedigitalacademy.co.za>
2019-01-05 11:49:13 +0200

    v1

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const bot = new BootBot({
 	appSecret: '21a5143ae019242c65d3d3f09202be45'
 });
 
-
+//default menu
 
 bot.hear('menu', (payload, chat) => {
 	chat.getUserProfile().then((user) => {
@@ -29,9 +29,9 @@ bot.hear('menu', (payload, chat) => {
 	});
 });
 
-
+//PROMOS
 bot.on('postback:PROMOS', (payload, chat) => {
-	chat.say(`History here...`);
+	chat.say(`Did you know?`);
 	axios.get('https://processor-module.firebaseapp.com/processor/v1/promotions')
 		.then(response => {
 			chat.say(response.data.advert);
@@ -39,39 +39,40 @@ bot.on('postback:PROMOS', (payload, chat) => {
 		});
 });
 
-//hardcoded for now
+//HISTORY 
 bot.on('postback:HISTORY', (payload, chat) => {
-	chat.conversation((convo) => {
-		axios.get('https://processor-module.firebaseapp.com/processor/v1/userIntents/27748834166')
-		.then(response => {
-console.log(response.data);
-				for(var i = 0; i < response.data.intents.length; i++) {
-						console.log(i + ' ' +  response.data.intents[i]);
-			
-					//	27748834166 = response.data.msidn
-				chat.say(i + ' - ' + response.data.intents[i]);
-	 
-				console.log(i + ' ) ' + response.data.intents[i]);
-			
-				console.log(response.data.intents)
-		}
-		
+	chat.getUserProfile().then((user) => {
+		axios.get('https://processor-module.firebaseapp.com/processor/v1/userIntents/' + user.id)
+			.then(response => {
+				console.log(response.data);
+
+				for (var i = 0; i < response.data.intents.length; i++) {
+					console.log(i + ' ' + response.data.intents[i]);
+
+					user.id = response.data.msidn
+					chat.say(i + ' - ' + response.data.intents[i]);
+
+					console.log(i + ' ) ' + response.data.intents[i]);
+
+					console.log(response.data.intents)
+				}
 
 
-		})
+
+			})
+
+		chat.say(`To go to the Main menu just enter “menu.”`);
+
+
+
 	})
-	chat.say(`coming soon...`);
-	
-		
 
 })
 
 
 
 
-
-
-
+//EXIT
 bot.on('postback:EXIT', (payload, chat) => {
 	chat.getUserProfile().then((user) => {
 		chat.say(`Bye ${user.first_name}! come back soon. \nTo go to the main menu just enter “menu.”`);
@@ -80,85 +81,166 @@ bot.on('postback:EXIT', (payload, chat) => {
 
 
 
-bot.hear(['hello', 'hi', /hey( there)?/i], (payload, chat) => {
+
+
+
+
+
+
+//BOT START FUNCTION ( CHECK IF USER EXIST OR NOT )
+
+bot.hear([/hello( there)?/i, /hey( there)?/i, /hi( there)?/i], (payload, chat) => {
 	chat.getUserProfile().then((user) => {
 		const text = payload.message.text;
 		console.log(user.id)
-		addUser.addUserDetails(user)
-		chat.say(`Hello, ${user.first_name}! `, { typing: true }).then(() => {
+		chat.say(`Y'ello, ${user.first_name}! `, { typing: true }).then(() => {
+			axios.get('https://processor-module.firebaseapp.com/processor/v1/userDetails/' + user.first_name)
+				.then(response => {
+					if (response.data.exists === true) {
+						console.log("[+] The user exists, routing to the default menu.");
+						console.log(response.data.msidn)
+						user.id = response.data.msidn
+						const text = payload.message.text;
+						const buttons = [
+							{ type: 'postback', title: 'promos', payload: 'PROMOS' },
+							{ type: 'postback', title: 'History', payload: 'HISTORY' },
+							{ type: 'postback', title: 'Exit', payload: 'EXIT' }
+						];
+						chat.sendButtonTemplate(`Y'ello ${user.first_name}!, How can i help you?`, buttons);
+
+						//PROMOS
+						bot.on('postback:PROMOS', (payload, chat) => {
+							chat.say(`Did you know?`);
+							axios.get('https://processor-module.firebaseapp.com/processor/v1/promotions')
+								.then(response => {
+									chat.say(response.data.advert);
+
+								});
+						});
+
+						//HISTORY 
+						bot.on('postback:HISTORY', (payload, chat) => {
+							chat.getUserProfile().then((user) => {
+								axios.get('https://processor-module.firebaseapp.com/processor/v1/userIntents/' + user.id)
+									.then(response => {
+										console.log(response.data);
+
+										for (var i = 0; i < response.data.intents.length; i++) {
+											console.log(i + ' ' + response.data.intents[i]);
+
+											user.id = response.data.msidn
+											chat.say(i + ' - ' + response.data.intents[i]);
+
+											console.log(i + ' ) ' + response.data.intents[i]);
+
+											console.log(response.data.intents)
+										}
 
 
-			const askNumber = (convo => {
-				convo.ask(`May you please type in your Phone number for registration?`, (payload, convo) => {
-					const text = payload.message.text;
-					convo.set('phone number', text);
-					convo.say('Thanks got it!').then(() => {
-						convo.ask({
-							text: `Is this your number: ${text}?`,
-							quickReplies: ['Yes', 'No']
-						}, (payload, convo) => {
-							const text = payload.message.text;
-							convo.say(`Thanks for confirming..`);
-							convo.end();
-						}, [
-								{
-									event: 'quick_reply',
-									callback: (payload, convo) => {
-										const text = payload.message.text;
-										convo.say(`Coolio, your number has been saved.`).then(() => askIntent(convo));
 
-									}
-								}
-							]);
-					});
+									})
+
+								chat.say(`To go to the Main menu just enter “menu.”`);
+
+
+
+							})
+
+						})
+
+
+
+
+						//EXIT
+						bot.on('postback:EXIT', (payload, chat) => {
+							chat.getUserProfile().then((user) => {
+								chat.say(`Bye ${user.first_name}! come back soon. \nTo go to the main menu just enter “menu.”`);
+							});
+						});
+
+
+					}
+					else {
+						console.log("[-] The user does not exist, routing to the registration scene.")
+						addUser.addUserDetails(user)
+						chat.say(`i see you are new :  ${user.first_name}!`).then(() => {
+							const askNumber = (convo => {
+								convo.ask(`May you please type in your Phone number for registration?`, (payload, convo) => {
+									const text = payload.message.text;
+									convo.set('phone number', text);
+									convo.say('Thanks got it!').then(() => {
+										convo.ask({
+											text: `Is this your number: ${text}?`,
+											quickReplies: ['Yes', 'No']
+										}, (payload, convo) => {
+											const text = payload.message.text;
+											convo.say(`Thanks for confirming..`);
+											convo.end();
+										}, [
+												{
+													event: 'quick_reply',
+													callback: (payload, convo) => {
+														const text = payload.message.text;
+														convo.say(`Coolio, your number has been saved.`).then(() => askIntent(convo));
+				
+													}
+												}
+											]);
+									});
+								});
+							});
+
+							const askIntent = (convo => {
+								convo.ask(`What would you like to do?`, (payload, convo) => {
+									const text = payload.message.text;
+									convo.set('intent', text)
+									convo.say('Processing...').then(() => {
+										convo.ask({
+											text: `You sure you want to purchase "${convo.get('intent')}" ?`,
+											quickReplies: ['Yup', 'Nop']
+										}, (payload, convo) => {
+											const text = payload.message.text;
+											convo.say(`Thanks for confirming..`);
+											convo.end();
+										}, [
+												{
+													event: 'quick_reply',
+													callback: (payload, convo) => {
+														const text = payload.message.text;
+														addIntent.addUserIntent(convo, chat)
+														console.log('user intent saved..')
+														convo.say(`Sharp, your request has been processed \nTo go main menu just enter “menu.”`)
+														convo.end();
+				
+													}
+												}
+											])
+									});
+				
+				
+								});
+							});
+
+
+
+							chat.conversation((convo) => {
+								askNumber(convo);
+								console.log('user details added...')
+							});
+
+						})
+					}
+
 				});
-			});
 
 
 
-			const askIntent = (convo => {
-				convo.ask(`What would you like to do?`, (payload, convo) => {
-					const text = payload.message.text;
-					convo.set('intent', text)
-					convo.say('Processing...').then(() => {
-						convo.ask({
-							text: `You sure you want to purchase "${convo.get('intent')}" ?`,
-							quickReplies: ['Yup', 'Nop']
-						}, (payload, convo) => {
-							const text = payload.message.text;
-							convo.say(`Thanks for confirming..`);
-							convo.end();
-						}, [
-								{
-									event: 'quick_reply',
-									callback: (payload, convo) => {
-										const text = payload.message.text;
-										addIntent.addUserIntent(convo,convo)
-										console.log('user intent saved..')
-										convo.say(`Sharp, your request has been processed \nTo go main menu just enter “menu.”`)
-										convo.end();
 
-									}
-								}
-							])
-					});
-
-
-				});
-			});
-
-
-			chat.conversation((convo) => {
-				askNumber(convo);
-				console.log('user details added...')
-			});
 		});
 
+	})
+})
 
-
-
-	});
-});
 
 
 
--- utils.js
+++ utils.js
@@ -7,8 +7,8 @@ function addUserDetails(user) {
 
     let user_ID = {
         
-        messenger_id: user.id,
-        first_name: user.first_name,
+        msidn: user.id,
+        telegram_id: user.first_name,
         last_name: user.last_name
     }
     console.log(user_ID);
@@ -25,32 +25,40 @@ function addUserDetails(user) {
 /*
 ADDING USER INTENT TO FIRESTORE AND CALLING IT TO (GET_INTENT_SCENE)
 */
-function addUserIntent(convo) {
 
-    // convo.get('phone number') = phoneNumber
 
-    let userIntent = {
+
+    function addUserIntent(convo, chat) {
+    
+        chat.getUserProfile().then((user) => {
+   
+        let userIntent = {
         
-        intent:  convo.get('intent') ,
-        msidn :convo.get('phone number')
-        //telegram_id: user.id
-
-
-    }
-    /*
-    CONSOLE THE USER INPUT / TEXT TYPED ON THE KEYBOARD
-    */
-   // console.log(userIntent.text);
-    /*
-    SEND USER INTENT TO SAVEUSER INTENTS END-POINT 
-    */
-    axios.post('https://processor-module.firebaseapp.com/processor/v1/saveuserIntents', userIntent)
-        .then(function (response) {
-            console.log(response.data);
-
-        })
+            intent:  convo.get('intent') ,
+            msidn : user.id
+            //telegram_id: user.id
+    
+    
+        }
+        /*
+        CONSOLE THE USER INPUT / TEXT TYPED ON THE KEYBOARD
+        */
+       // console.log(userIntent.text);
+        /*
+        SEND USER INTENT TO SAVEUSER INTENTS END-POINT 
+        */
+        axios.post('https://processor-module.firebaseapp.com/processor/v1/saveuserIntents', userIntent)
+            .then(function (response) {
+                console.log(response.data);
+    
+            })
+   
+        })  
+    
 
 }
+
+
 
 module.exports = {
     addUserDetails: addUserDetails,
